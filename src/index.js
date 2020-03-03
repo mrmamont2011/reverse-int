b7ceb4474227eeff6ad15e9dee32190331ef7a74
mrmamont2011 <60566644+mrmamont2011@users.noreply.github.com>
2020-03-03 12:55:21 +0300

    feat: tasks reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  let str = String(Math.abs(n));
+  let out_str ='';
+  for (let i=0; i<str.length; i++) out_str += str[str.length-1-i];
+  return (Number(out_str));
+
 }
